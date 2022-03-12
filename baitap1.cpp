4cbd4d9b6dfe5f722c567214a1da8c6ac8409e4d
Atermis07 <101449795+Atermis07@users.noreply.github.com>
2022-03-12 15:35:50 +0700

    Add files via upload

---
+++ baitap1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "ho va ten" << " Tang Quoc Thinh"<< endl;
+    cout << "mssv " <<"106210040 "<< endl;
+    cout << "lop "<< "21DTCLC1"<< endl;
+    return 0;
+}
